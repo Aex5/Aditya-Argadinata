fe6f4744c009cb898678d41a049d549b8b06b9b2
Sujaruu <ytfuga02@gmail.com>
2022-10-02 21:18:16 +0700

    fix: debug some bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <nav className="w-full h-16 flex justify-start items-center px-8 fixed bg-[#fbfcfd] opacity-95 border-t-2 z-50">
+      <nav className="w-full h-16 flex justify-start items-center px-8 fixed bg-[#fbfcfd] bg-opacity-80 backdrop-blur-md border-t-2 z-50">
         <div className="navbar w-full max-w-[900px] mx-auto md:px-8">
           <ul className="flex gap-5 text-md text-gray-500">
             <Link href="/">
