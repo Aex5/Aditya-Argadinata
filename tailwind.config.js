2160b6d44b290f0cf3ee1544ad593d48251e1bdd
Sujaruu <ytfuga02@gmail.com>
2022-10-19 20:02:26 +0700

    feat: config for next theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.tsx"],
+    options: {
+      safelist: ["dark"], //specific classes
+    },
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -9,6 +17,21 @@ module.exports = {
     backgroundImage: {
       "glare-ef": "url('../public/glare.png')",
     },
+    typography: (theme) => ({}),
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: "white",
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  variants: {
+    variants: {
+      typography: ["dark"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
